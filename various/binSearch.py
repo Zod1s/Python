a3abb3ae9ad97524972e4fc87dcaf13509843385
Zod1s <lorefrance15@gmail.com>
2021-01-12 22:16:39 +0100

    Varie

---
+++ various/binSearch.py
@@ -0,0 +1,29 @@
+def iterBinSearch(ls, t):
+    low = 0
+    high = len(ls)
+    while low < high:
+        mid = (low + high) // 2
+        if ls[mid] == t:
+            return mid
+        elif ls[mid] < t:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return len(ls)
+
+def recBinSearch(ls, t, low, high):
+    if low > high:
+        return len(ls)
+    mid = (low + high) // 2
+    if ls[mid] == t:
+        return mid
+    elif ls[mid] < t:
+        return recBinSearch(ls, t, mid + 1, high)
+    else:
+        return recBinSearch(ls, t, low, mid - 1)
+
+a = [1, 2, 3, 8, 10, 12, 22, 23, 24, 25]
+b = 3
+i = iterBinSearch(a, b)
+# i = recBinSearch(a, b, 0, len(a))
+print(f"Elem a[{i}] = {a[i]}, should be {b}")
